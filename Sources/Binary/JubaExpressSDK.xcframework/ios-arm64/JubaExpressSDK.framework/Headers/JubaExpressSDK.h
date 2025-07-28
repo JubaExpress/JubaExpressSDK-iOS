1afb18be7503aef47dfaf2e11b3bc404fb74482f
Muhammad kumail <muhammad.kumail@jubaexpress.com>
2025-07-28 15:40:22 +0400

    Add Swift Package Manager support

---
+++ Sources/Binary/JubaExpressSDK.xcframework/ios-arm64/JubaExpressSDK.framework/Headers/JubaExpressSDK.h
@@ -0,0 +1,18 @@
+//
+//  JESDK.h
+//  JESDK
+//
+//  Created by Muhammad kumail on 15/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JESDK.
+FOUNDATION_EXPORT double JESDKVersionNumber;
+
+//! Project version string for JESDK.
+FOUNDATION_EXPORT const unsigned char JESDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JESDK/PublicHeader.h>
+
+
